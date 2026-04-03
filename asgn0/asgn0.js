98d3a56f502a795b2766f5325e3621703f0784b7
Sam Gallo <saangall@ucsc.edu>
2026-04-03 15:13:31 -0700

    added drawing the standard lines. next, the operations

--- asgn0/asgn0.js
+++ asgn0/asgn0.js
@@ -13,10 +13,32 @@ function main() {
   // Draw a blue rectangle
   ctx.fillStyle = 'rgba(0, 0, 0, 1.0)'; // Set color to blue
   ctx.fillRect(0, 0, canvas.width, canvas.height);        // Fill a rectangle with the color
-  let v1 = new Vector3(2.5,2.5,0);
 }
 
 function handleDrawEvent(){
-  let v1 = document.getElementById('vector1x').value;
-  console.log(v1);
+  main();
+  let v1x = document.getElementById('vector1x').value;
+  let v1y = document.getElementById('vector1y').value;
+  let v1 = new Vector3([v1x, v1y, 0]);
+  //let v1 = new Vector3([2.5, 2.5, 0]);
+  console.log("Yooooo");
+  drawVector(v1, 'red');
+
+  let v2x = document.getElementById('vector2x').value;
+  let v2y = document.getElementById('vector2y').value;
+  let v2 = new Vector3([v2x, v2y, 0]);
+  console.log("Sup");
+  drawVector(v2, 'blue');
+}
+
+function drawVector(v, color){
+  var canvas = document.getElementById('example');
+  var ctx = canvas.getContext('2d');
+  ctx.strokeStyle = color;
+  let cx = canvas.width/2;
+  let cy = canvas.height/2;
+  ctx.beginPath();
+  ctx.moveTo(cx, cy);
+  ctx.lineTo(cx + (v.elements[0] * 20), cy - (v.elements[1] * 20));
+  ctx.stroke();
 }
